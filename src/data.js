40601362aa291d8b4d0633f7b314bdc0b95d0567
egad13 <egad.coding@gmail.com>
2023-11-07 11:33:11 -0500

    fix: Add new Riot gene

--- src/data.js
+++ src/data.js
@@ -581,6 +581,7 @@ export const GENES = deepFreeze({
 		gene("Pufferfish", UNCOMMON, false, [UNDERTIDE]),
 		gene("Remora", RARE, false, [UNDERTIDE]),
 		gene("Ringlets", UNCOMMON, true, [BANESCALE, GAOLER, UNDERTIDE]),
+		gene("Riot", LIMITED, false, [ABERRATION, GAOLER]),
 		gene("Runes", UNCOMMON, true, [GAOLER, UNDERTIDE, VEILSPUN]),
 		gene("Sailfin", RARE, false, [UNDERTIDE]),
 		gene("Scales", LIMITED, true, [ABERRATION, AETHER]),
